374b742d9ba1cb970a1bc165a925fbe985579aef
Deyang Kong <33001824+kdyang@users.noreply.github.com>
2019-05-13 15:01:43 +0800

    Add files via upload

--- coil_icra.py
+++ coil_icra.py
@@ -5,14 +5,17 @@ from logger import coil_logger
 import torch.nn as nn
 import torch
 import importlib
+import torch.nn as nn
+import torch.nn.init as init
+import torch.nn.functional as F
 
 from configs import g_conf
 from coilutils.general import command_number_to_index
 
-from building_blocks import Conv
-from building_blocks import Branching
-from building_blocks import FC
-from building_blocks import Join
+from .building_blocks import Conv
+from .building_blocks import Branching
+from .building_blocks import FC
+from .building_blocks import Join
 
 class CoILICRA(nn.Module):
 
@@ -22,46 +25,10 @@ class CoILICRA(nn.Module):
         super(CoILICRA, self).__init__()
         self.params = params
 
-        number_first_layer_channels = 0
-
-        for _, sizes in g_conf.SENSORS.items():
-            number_first_layer_channels += sizes[0] * g_conf.NUMBER_FRAMES_FUSION
-
-        # Get one item from the dict
-        sensor_input_shape = next(iter(g_conf.SENSORS.values()))
-        sensor_input_shape = [number_first_layer_channels, sensor_input_shape[1],
-                              sensor_input_shape[2]]
-
-        # For this case we check if the perception layer is of the type "conv"
-        if 'conv' in params['perception']:
-            perception_convs = Conv(params={'channels': [number_first_layer_channels] +
-                                                          params['perception']['conv']['channels'],
-                                            'kernels': params['perception']['conv']['kernels'],
-                                            'strides': params['perception']['conv']['strides'],
-                                            'dropouts': params['perception']['conv']['dropouts'],
-                                            'end_layer': True})
-
-            perception_fc = FC(params={'neurons': [perception_convs.get_conv_output(sensor_input_shape)]
-                                                  + params['perception']['fc']['neurons'],
-                                       'dropouts': params['perception']['fc']['dropouts'],
-                                       'end_layer': False})
-
-            self.perception = nn.Sequential(*[perception_convs, perception_fc])
-
-            number_output_neurons = params['perception']['fc']['neurons'][-1]
-
-        elif 'res' in params['perception']:  # pre defined residual networks
-            resnet_module = importlib.import_module('network.models.building_blocks.resnet')
-            resnet_module = getattr(resnet_module, params['perception']['res']['name'])
-            self.perception = resnet_module(pretrained=g_conf.PRE_TRAINED,
-                                             num_classes=params['perception']['res']['num_classes'])
-
-            number_output_neurons = params['perception']['res']['num_classes']
-
-        else:
-
-            raise ValueError("invalid convolution layer type")
-
+        self.segment=ERFNet(num_classes)
+        for param in self.segment.parameters():
+            param.requires_grad = False
+        self.policy=CarlaNet()
         self.measurements = FC(params={'neurons': [len(g_conf.INPUTS)] +
                                                    params['measurements']['fc']['neurons'],
                                        'dropouts': params['measurements']['fc']['dropouts'],
@@ -121,7 +88,7 @@ class CoILICRA(nn.Module):
         branch_outputs = self.branches(j)
 
         speed_branch_output = self.speed_branch(x)
-
+        
         # We concatenate speed with the rest.
         return branch_outputs + [speed_branch_output]
 
@@ -160,5 +127,322 @@ class CoILICRA(nn.Module):
                                      torch.cuda.LongTensor(range(0, len(branch_number)))])
 
         return output_vec[branch_number[0], branch_number[1], :]
+
+class CarlaNet(nn.Module):
+    def __init__(self, dropout_vec=None):
+        super(CarlaNet, self).__init__()
+        self.conv_block = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=5, stride=2),
+            nn.BatchNorm2d(32),
+            # nn.Dropout(self.dropout_vec[0]),
+            nn.ReLU(),
+            nn.Conv2d(32, 32, kernel_size=3, stride=1),
+            nn.BatchNorm2d(32),
+            # nn.Dropout(self.dropout_vec[1]),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=3, stride=2),
+            nn.BatchNorm2d(64),
+            # nn.Dropout(self.dropout_vec[2]),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.BatchNorm2d(64),
+            # nn.Dropout(self.dropout_vec[3]),
+            nn.ReLU(),
+            nn.Conv2d(64, 128, kernel_size=3, stride=2),
+            nn.BatchNorm2d(128),
+            # nn.Dropout(self.dropout_vec[4]),
+            nn.ReLU(),
+            nn.Conv2d(128, 128, kernel_size=3, stride=1),
+            nn.BatchNorm2d(128),
+            # nn.Dropout(self.dropout_vec[5]),
+            nn.ReLU(),
+            nn.Conv2d(128, 256, kernel_size=3, stride=1),
+            nn.BatchNorm2d(256),
+            # nn.Dropout(self.dropout_vec[6]),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1),
+            nn.BatchNorm2d(256),
+            # nn.Dropout(self.dropout_vec[7]),
+            nn.ReLU(),
+        )
+
+        self.img_fc = nn.Sequential(
+                nn.Linear(8192, 512),
+                nn.Dropout(0.3),
+                nn.ReLU(),
+                nn.Linear(512, 512),
+                nn.Dropout(0.3),
+                nn.ReLU(),
+            )
+
+        self.speed_fc = nn.Sequential(
+                nn.Linear(1, 128),
+                nn.Dropout(0.5),
+                nn.ReLU(),
+                nn.Linear(128, 128),
+                nn.Dropout(0.5),
+                nn.ReLU(),
+            )
+
+        self.emb_fc = nn.Sequential(
+                nn.Linear(512+128, 512),
+                nn.Dropout(0.5),
+                nn.ReLU(),
+            )
+
+        self.control_branches = nn.ModuleList([
+            nn.Sequential(
+                nn.Linear(512, 256),
+                nn.Dropout(0.5),
+                nn.ReLU(),
+                nn.Linear(256, 256),
+                # nn.Dropout(self.dropout_vec[i*2+14]),
+                nn.ReLU(),
+                nn.Linear(256, 3),
+            ) for i in range(4)
+        ])
+
+        self.speed_branch = nn.Sequential(
+                nn.Linear(512, 256),
+                nn.Dropout(0.5),
+                nn.ReLU(),
+                nn.Linear(256, 256),
+                # nn.Dropout(self.dropout_vec[1]),
+                nn.ReLU(),
+                nn.Linear(256, 1),
+            )
+
+    def forward(self, img, speed):
+        img = self.conv_block(img)
+        img = img.view(-1, 8192)
+        img = self.img_fc(img)
+
+        speed = self.speed_fc(speed)
+        speed =speed.view(-1,128)
+        emb = torch.cat([img, speed], dim=1)
+        emb = self.emb_fc(emb)
+
+        pred_control = torch.cat(
+            [out(emb) for out in self.control_branches], dim=1)
+        pred_speed = self.speed_branch(img)
+
+        return pred_control, pred_speed, img, emb
+
+class UncertainNet(nn.Module):
+    def __init__(self, structure=2, dropout_vec=None):
+        super(UncertainNet, self).__init__()
+        self.structure = structure
+
+        if (self.structure < 2 or self.structure > 3):
+            raise("Structure must be one of 2|3")
+
+        self.uncert_speed_branch = nn.Sequential(
+                nn.Linear(512, 256),
+                nn.ReLU(),
+                nn.Linear(256, 256),
+                nn.ReLU(),
+                nn.Linear(256, 1),
+            )
+        if self.structure == 2:
+            self.uncert_control_branches = nn.ModuleList([
+                nn.Sequential(
+                    nn.Linear(512, 256),
+                    nn.ReLU(),
+                    nn.Linear(256, 256),
+                    nn.ReLU(),
+                    nn.Linear(256, 3),
+                ) for i in range(4)
+            ])
+
+        if self.structure == 3:
+            self.uncert_control_branches = nn.Sequential(
+                    nn.Linear(512, 256),
+                    nn.ReLU(),
+                    nn.Linear(256, 256),
+                    nn.ReLU(),
+                    nn.Linear(256, 3),
+            )
+
+    def forward(self, img_emb, emb):
+        if self.structure == 2:
+            log_var_control = torch.cat(
+                [un(emb) for un in self.uncert_control_branches], dim=1)
+        if self.structure == 3:
+            log_var_control = self.uncert_control_branches(emb)
+            log_var_control = torch.cat([log_var_control for _ in range(4)],
+                                        dim=1)
+
+        log_var_speed = self.uncert_speed_branch(img_emb)
+
+        return log_var_control, log_var_speed
+
+class FinalNet(nn.Module):
+    def __init__(self, structure=2, dropout_vec=None):
+        super(FinalNet, self).__init__()
+        self.structure = structure
+
+        self.carla_net = CarlaNet(dropout_vec=dropout_vec)
+        self.uncertain_net = UncertainNet(structure)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(
+                    m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.xavier_uniform_(
+                    m.weight)
+                m.bias.data.fill_(0.01)
+
+    def forward(self, img, speed):
+        pred_control, pred_speed, img_emb, emb = self.carla_net(img, speed)
+        log_var_control, log_var_speed = self.uncertain_net(img_emb, emb)
+        print(pred_control.size())        
+        print(pred_speed.size())
+        print(log_var_control.size())
+        print(log_var_speed.size())
+        return pred_control, pred_speed, log_var_control, log_var_speed
+
+class DownsamplerBlock (nn.Module):
+    def __init__(self, ninput, noutput):
+        super().__init__()
+
+        self.conv = nn.Conv2d(ninput, noutput-ninput, (3, 3), stride=2, padding=1, bias=True)
+        self.pool = nn.MaxPool2d(2, stride=2)
+        self.bn = nn.BatchNorm2d(noutput, eps=1e-3)
+
+    def forward(self, input):
+        output = torch.cat([self.conv(input), self.pool(input)], 1)
+        output = self.bn(output)
+        return F.relu(output)
+    
+class non_bottleneck_1d (nn.Module):
+    def __init__(self, chann, dropprob, dilated):        
+        super().__init__()
+
+        self.conv3x1_1 = nn.Conv2d(chann, chann, (3, 1), stride=1, padding=(1,0), bias=True)
+
+        self.conv1x3_1 = nn.Conv2d(chann, chann, (1,3), stride=1, padding=(0,1), bias=True)
+
+        self.bn1 = nn.BatchNorm2d(chann, eps=1e-03)
+
+        self.conv3x1_2 = nn.Conv2d(chann, chann, (3, 1), stride=1, padding=(1*dilated,0), bias=True, dilation = (dilated,1))
+
+        self.conv1x3_2 = nn.Conv2d(chann, chann, (1,3), stride=1, padding=(0,1*dilated), bias=True, dilation = (1, dilated))
+
+        self.bn2 = nn.BatchNorm2d(chann, eps=1e-03)
+
+        self.dropout = nn.Dropout2d(dropprob)
+        
+
+    def forward(self, input):
+
+        output = self.conv3x1_1(input)
+        output = F.relu(output)
+        output = self.conv1x3_1(output)
+        output = self.bn1(output)
+        output = F.relu(output)
+
+        output = self.conv3x1_2(output)
+        output = F.relu(output)
+        output = self.conv1x3_2(output)
+        output = self.bn2(output)
+
+        if (self.dropout.p != 0):
+            output = self.dropout(output)
+        
+        return F.relu(output+input)    #+input = identity (residual connection)
+
+
+class Encoder(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.initial_block = DownsamplerBlock(3,16)
+
+        self.layers = nn.ModuleList()
+
+        self.layers.append(DownsamplerBlock(16,64))
+
+        for x in range(0, 5):    #5 times
+           self.layers.append(non_bottleneck_1d(64, 0.03, 1)) 
+
+        self.layers.append(DownsamplerBlock(64,128))
+
+        for x in range(0, 2):    #2 times
+            self.layers.append(non_bottleneck_1d(128, 0.3, 2))
+            self.layers.append(non_bottleneck_1d(128, 0.3, 4))
+            self.layers.append(non_bottleneck_1d(128, 0.3, 8))
+            self.layers.append(non_bottleneck_1d(128, 0.3, 16))
+
+        #Only in encoder mode:
+        self.output_conv = nn.Conv2d(128, num_classes, 1, stride=1, padding=0, bias=True)
+
+    def forward(self, input, predict=False):
+        output = self.initial_block(input)
+
+        for layer in self.layers:
+            output = layer(output)
+
+        if predict:
+            output = self.output_conv(output)
+
+        return output
+
+
+class UpsamplerBlock (nn.Module):
+    def __init__(self, ninput, noutput):
+        super().__init__()
+        self.conv = nn.ConvTranspose2d(ninput, noutput, 3, stride=2, padding=1, output_padding=1, bias=True)
+        self.bn = nn.BatchNorm2d(noutput, eps=1e-3)
+
+    def forward(self, input):
+        output = self.conv(input)
+        output = self.bn(output)
+        return F.relu(output)
+
+class Decoder (nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+
+        self.layers = nn.ModuleList()
+
+        self.layers.append(UpsamplerBlock(128,64))
+        self.layers.append(non_bottleneck_1d(64, 0, 1))
+        self.layers.append(non_bottleneck_1d(64, 0, 1))
+
+        self.layers.append(UpsamplerBlock(64,16))
+        self.layers.append(non_bottleneck_1d(16, 0, 1))
+        self.layers.append(non_bottleneck_1d(16, 0, 1))
+
+        self.output_conv = nn.ConvTranspose2d( 16, num_classes, 2, stride=2, padding=0, output_padding=0, bias=True)
+
+    def forward(self, input):
+        output = input
+
+        for layer in self.layers:
+            output = layer(output)
+
+        output = self.output_conv(output)
+
+        return output
+#ERFNet
+class ERFNet(nn.Module):
+    def __init__(self, num_classes, encoder=None):  #use encoder to pass pretrained encoder
+        super().__init__()
+
+        if (encoder == None):
+            self.encoder = Encoder(num_classes)
+        else:
+            self.encoder = encoder
+        self.decoder = Decoder(num_classes)
+
+    def forward(self, input, only_encode=False):
+        if only_encode:
+            return self.encoder.forward(input, predict=True)
+        else:
+            output = self.encoder(input)    #predict=False by default
+            return self.decoder.forward(output)
 
 
